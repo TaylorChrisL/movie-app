48376ec6989fa2e366f0a587100cb42ae0117b66
Chris Taylor <ctaylor534@gmail.com>
2022-07-06 14:31:54 -0700

    Changes routes and controllers to restful convention

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,4 +10,33 @@ class MoviesController < ApplicationController
     render json: movie.as_json
   end
 
+  def create
+    movie = Movie.new(
+      title: params["title"],
+      year: params["year"],
+      plot: params["plot"],
+    )
+
+    movie.save
+    render json: movie
+  end
+
+  def update
+    movie = Movie.find_by(id: params["id"])
+
+    movie.title = params["title"] || movie.title
+    movie.year = params["year"] || movie.year
+    movie.plot = params["plot"] || movie.plot
+
+    movie.save
+    render json: movie
+  end
+
+  def destroy
+    movie = Movie.find_by(id: params["id"])
+    movie.destroy
+
+    render json: {message: "Successfully Obliterated!"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  
+  resources :actors, :movies, defaults: { format: :json }
+  # old stuff
+  # get "/actors", controller: "actors", action: "show_actors"
 
-  get "/actors", controller: "actors", action: "show_actors"
+  # get "/actor/:id", controller: "actors", action: "show_actor"
 
-  get "/actor/:id", controller: "actors", action: "show_actor"
+  # get "/actor" => "actors#show_actor"
 
-  get "/actor" => "actors#show_actor"
+  # get "/movies" => "movies#index"
 
-  get "/movies" => "movies#index"
-
-  get "/movies/:id" => "movies#show"
+  # get "/movies/:id" => "movies#show"
 end
