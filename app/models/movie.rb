95287cb3dad55001d045585dbfcd8576721349cf
Chris Taylor <ctaylor534@gmail.com>
2022-07-12 21:04:38 -0700

    Adds association between actors and movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors
   validates :title, presence: true, length: { minimum: 2}
   validates :year, numericality: true
   validates :plot, length: { in: 10..500 }
